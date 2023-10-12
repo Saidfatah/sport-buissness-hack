3e2f3e4bdc4a3dbe75e0ec9cded902f2bb1635b2
ThePainter <59207693+Saidfatah@users.noreply.github.com>
2023-10-12 11:48:17 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./styles.css";
+import { GoButton } from "./GoButton";
+
+export default function App() {
+  return (
+    <div className="container mx-auto w-4/5 my-10">
+      <GoButton />
+    </div>
+  );
+}
---
+++ src/GoButton.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const GoButton = () => {
+  return (
+    <button>
+      <span>G</span>
+      <span>O</span>
+    </button>
+  );
+};
